f51efef0a7a682d7789a502e277dfd7ec7eb5365
LazzariDev <lazzaridev@gmail.com>
2023-12-26 01:11:24 +0100

    PocketBase

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,10 +1,33 @@
+import { Product } from '@/model/product';
+import { pb } from '@/pocketbase';
+import { useEffect, useState } from 'react';
 
 export function ShopPage() {
+    const [ products, setProducts ] = useState<Product[]>([]);
+
+    useEffect(() => {
+        loadData();
+    }, [])
+
+    function loadData() {
+        pb.collection('products').getList<Product>()
+            .then(res => {
+                setProducts(res.items)
+            })
+    }
+
     return (
         <div>
             <h1 className="title"> SHOP </h1>
 
-            content here...
+            {
+                products.map(p => (
+                    <li key={p.id}>
+                        {p.name}
+                    </li>
+                ))
+            }
+
         </div>
     )
 }
